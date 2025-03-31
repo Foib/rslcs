c0f551fc4715adf18fc28b53a0662b72abf4c124
Fynn Budde <foiblp@gmail.com>
2025-03-31 16:35:00 +0200

    Initial commit

---
+++ rslcs/Result.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace rslcs
+{
+    public class Result<T>
+    {
+        [MemberNotNullWhen(false, nameof(Error))]
+        [MemberNotNullWhen(true, nameof(Value))]
+        public bool IsSuccess { get; }
+        public T? Value { get; }
+        public Exception? Error { get; }
+
+        private Result(T value)
+        {
+            Value = value;
+            Error = null;
+            IsSuccess = true;
+        }
+
+        private Result(Exception error)
+        {
+            Value = default;
+            Error = error;
+            IsSuccess = false;
+        }
+
+        public static Result<T> Ok(T value) => new(value);
+
+        public static Result<T> Err(Exception error) => new(error);
+
+        public void Deconstruct(out bool isSuccess, out T? value, out Exception? error)
+        {
+            isSuccess = IsSuccess;
+            value = Value;
+            error = Error;
+        }
+    }
+}
---
+++ rslcs/ResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace rslcs
+{
+    public static class ResultExtensions
+    {
+        public static Result<TOut> Then<TIn, TOut>(this Result<TIn> result, Func<TIn, Result<TOut>> nextOperation)
+        {
+            if (!result.IsSuccess)
+                return Result<TOut>.Err(result.Error);
+
+            return nextOperation(result.Value);
+        }
+    }
+}
